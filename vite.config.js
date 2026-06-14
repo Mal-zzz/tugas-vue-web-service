687962c325087a2844ad65581d70a8045ef7fa35
Mal-zzz <akmalsyauqi222324@gmail.com>
2026-06-14 16:27:47 +0700

    Menambahkan konfigurasi GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/tugas-vue-web-service/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
